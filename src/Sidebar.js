2b2910868e973d433ef760e12a75564c8d22fe6a
zekiunyildiz <zekiunyildiz@gmail.com>
2021-09-19 20:24:45 +0300

    sortedNotes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,11 @@ function Sidebar({
   activeNote,
   setActiveNote,
 }) {
+
+
+  const sortedNotes = notes.sort((a,b)=>b.lastModified - a.lastModified);
+
+
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
@@ -13,7 +18,7 @@ function Sidebar({
       </div>
 
       <div className="app-sidebar-notes">
-        {notes.map((note) => (
+        {sortedNotes.map((note) => (
           <div
             className= {`app-sidebar-note ${note.id === activeNote && "active"}`}
             onClick={() => setActiveNote(note.id)}
